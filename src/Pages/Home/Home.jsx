c79fb7042e16ddbc3a3a9373071d75c2e13a011c
prithviraj <prithvirajpu999@gmail.com>
2025-10-24 19:14:08 +0530

    initial commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Home.css'
+import Navbar from '../../Components/Navbar/Navbar.jsx'
+import hero from '../../assets/hero_banner.jpg'
+import hero_title from '../../assets/hero_title.png'
+
+const Home = () => {
+  return (
+    <div className='Home'>
+      <Navbar />
+      <div className="hero">
+        <img src={hero} alt="" className='banner-img'/>
+        <div className="hero-caption">
+          <img src={hero_title} alt="" className='caption-img'/>
+          <p >Discovering his ties to a secret ancient order, a young 
+            man living in modern Istanbul embarks on a quest to save the
+             city from an immortal enemy</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
